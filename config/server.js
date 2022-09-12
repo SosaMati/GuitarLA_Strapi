5dd4d9fe0ff88577c6e5c7eb6df8cecbd3e9887b
SosaMati <matiasezequielsosa@gmail.com>
2022-09-12 01:57:23 -0300

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fc25bf18a756ee839e9d7e42dfca3f77'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9892cb02-50b4-463c-b24b-ba9a4634cca1'
+};
